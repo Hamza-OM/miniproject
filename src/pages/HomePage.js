d7c8688e58d122375d8e1fff78bdd85e6629fac6
hamza <hamzaalsiyabi957@gmail.com>
2024-07-09 13:06:42 +0400

    app commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, Carousel, NavLink } from 'react-bootstrap';
 import '../style.css'; // Assuming your custom styles are in style.css
-// import Footer from './Footer';
 import image1 from '../image1.jpg'; // Import the images
 import image2 from '../image2.jpg'; // Import the images
 import 'animate.css/animate.min.css';
-
 
 function HomePage() {
   return (
@@ -20,8 +18,10 @@ function HomePage() {
           />
           <Carousel.Caption>
             {/* <h3>Slide 1 Heading</h3>
-            <p>Slide 1 Description</p> */}
-            <NavLink to="/products" className="btn">Shop Now</NavLink>
+              <p>Slide 1 Description</p> */}
+            <NavLink to="/products" className="btn btn-primary">
+              Shop Now
+            </NavLink>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -32,8 +32,10 @@ function HomePage() {
           />
           <Carousel.Caption>
             {/* <h3>Slide 2 Heading</h3>
-            <p>Slide 2 Description</p> */}
-            <NavLink to="/products" className="btn">Explore More</NavLink>
+              <p>Slide 2 Description</p> */}
+            <NavLink to="/products" className="btn btn-secondary">
+              Explore More
+            </NavLink>
           </Carousel.Caption>
         </Carousel.Item>
         {/* Add more Carousel.Item elements for additional slides */}
@@ -42,43 +44,36 @@ function HomePage() {
       {/* Rest of your homepage content */}
       <Row>
         <Col>
-          {/* Your content goes here */}
+          {/* Your main content goes here */}
         </Col>
       </Row>
 
-      <footer className="footer fixed-bottom bg-light py-1 border-top"> {/* Reduced padding */}
-        <div className="container">
-          <div className="row justify-content-between align-items-center">
-            <div className="col-md-4">
-              <h5>Hamza Alsiyabi</h5>
-              <p>About Us</p>
-              <p>Contact Us</p>
-            </div>
-            <div className="col-md-4">
-              <h5>Quick Links</h5>
-              <p>Home</p>
-              <p>Register</p>
-              <p>Products</p>
-            </div>
-            <div className="col-md-4">
-              <h5>Follow Us</h5>
-              <p><i className="bi bi-twitter"></i> Twitter</p>
-              <p><i className="bi bi-instagram"></i> Instagram</p>
-              <p><i className="bi bi-facebook"></i> Facebook</p>
-            </div>
-          </div>
-          <div className="row mt-1"> {/* Reduced margin (optional) */}
-            <div className="col text-center">
-              <p>&copy; 2024 Company, Inc</p>
-            </div>
-          </div>
+      {/* Improved Footer */}
+      <footer className="footer fixed-bottom bg-light py-2 border-top d-flex justify-content-between align-items-center">
+        <div className="col-md-4">
+          <h5>Hamza Alsiyabi</h5>
+          <p>About Us</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="col-md-4">
+          <h5>Quick Links</h5>
+          <p>Home</p>
+          <p>Register</p>
+          <p>Products</p>
+        </div>
+        <div className="col-md-4 text-end">
+          <h5>Follow Us</h5>
+          <p>
+            <i className="bi bi-twitter"></i> Twitter
+          </p>
+          <p>
+            <i className="bi bi-instagram"></i> Instagram
+          </p>
+          <p>
+            <i className="bi bi-facebook"></i> Facebook
+          </p>
         </div>
       </footer>
-      <div>
-
-
-
-      </div>
     </Container>
   );
 }
