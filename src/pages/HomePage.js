aa56ada721d4d9a9e101d23fe65ff70d10c35312
hamza <hamzaalsiyabi957@gmail.com>
2024-07-09 12:46:21 +0400

    app commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -46,34 +46,34 @@ function HomePage() {
         </Col>
       </Row>
 
-      <footer className="footer fixed-bottom bg-light py-4 border-top">
-      <div className="container">
-        <div className="row justify-content-between align-items-center">
-          <div className="col-md-4">
-            <h5>Hamza Alsiyabi</h5>
-            <p>About Us</p>
-            <p>Contact Us</p>
+      <footer className="footer fixed-bottom bg-light py-1 border-top"> {/* Reduced padding */}
+        <div className="container">
+          <div className="row justify-content-between align-items-center">
+            <div className="col-md-4">
+              <h5>Hamza Alsiyabi</h5>
+              <p>About Us</p>
+              <p>Contact Us</p>
+            </div>
+            <div className="col-md-4">
+              <h5>Quick Links</h5>
+              <p>Home</p>
+              <p>Register</p>
+              <p>Products</p>
+            </div>
+            <div className="col-md-4">
+              <h5>Follow Us</h5>
+              <p><i className="bi bi-twitter"></i> Twitter</p>
+              <p><i className="bi bi-instagram"></i> Instagram</p>
+              <p><i className="bi bi-facebook"></i> Facebook</p>
+            </div>
           </div>
-          <div className="col-md-4">
-            <h5>Quick Links</h5>
-            <p>Home</p>
-            <p>Register</p>
-            <p>Products</p>
-          </div>
-          <div className="col-md-4">
-            <h5>Follow Us</h5>
-            <p><i className="bi bi-twitter"></i> Twitter</p>
-            <p><i className="bi bi-instagram"></i> Instagram</p>
-            <p><i className="bi bi-facebook"></i> Facebook</p>
+          <div className="row mt-1"> {/* Reduced margin (optional) */}
+            <div className="col text-center">
+              <p>&copy; 2024 Company, Inc</p>
+            </div>
           </div>
         </div>
-        <div className="row mt-3">
-          <div className="col text-center">
-            <p>&copy; 2024 Company, Inc</p>
-          </div>
-        </div>
-      </div>
-    </footer>
+      </footer>
     </Container>
   );
 }
