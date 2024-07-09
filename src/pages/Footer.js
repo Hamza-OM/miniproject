71bc5b5edd238b4e864d0a9b32382e92a49e2334
hamza <hamzaalsiyabi957@gmail.com>
2024-07-09 11:19:21 +0400

    app commit

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,37 +1,37 @@
-import React from 'react';
+// import React from 'react';
 
 
-const Footer = () => {
-  return (
-    <footer className="footer fixed-bottom bg-light py-4 border-top">
-      <div className="container">
-        <div className="row justify-content-between align-items-center">
-          <div className="col-md-4">
-            <h5>Hamza Alsiyabi</h5>
-            <p>About Us</p>
-            <p>Contact Us</p>
-          </div>
-          <div className="col-md-4">
-            <h5>Quick Links</h5>
-            <p>Home</p>
-            <p>Register</p>
-            <p>Products</p>
-          </div>
-          <div className="col-md-4">
-            <h5>Follow Us</h5>
-            <p><i className="bi bi-twitter"></i> Twitter</p>
-            <p><i className="bi bi-instagram"></i> Instagram</p>
-            <p><i className="bi bi-facebook"></i> Facebook</p>
-          </div>
-        </div>
-        <div className="row mt-3">
-          <div className="col text-center">
-            <p>&copy; 2024 Company, Inc</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
-};
+// const Footer = () => {
+//   return (
+//     <footer className="footer fixed-bottom bg-light py-4 border-top">
+//       <div className="container">
+//         <div className="row justify-content-between align-items-center">
+//           <div className="col-md-4">
+//             <h5>Hamza Alsiyabi</h5>
+//             <p>About Us</p>
+//             <p>Contact Us</p>
+//           </div>
+//           <div className="col-md-4">
+//             <h5>Quick Links</h5>
+//             <p>Home</p>
+//             <p>Register</p>
+//             <p>Products</p>
+//           </div>
+//           <div className="col-md-4">
+//             <h5>Follow Us</h5>
+//             <p><i className="bi bi-twitter"></i> Twitter</p>
+//             <p><i className="bi bi-instagram"></i> Instagram</p>
+//             <p><i className="bi bi-facebook"></i> Facebook</p>
+//           </div>
+//         </div>
+//         <div className="row mt-3">
+//           <div className="col text-center">
+//             <p>&copy; 2024 Company, Inc</p>
+//           </div>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
 
-export default Footer;
+// export default Footer;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Carousel, NavLink } from 'react-bootstrap';
 import '../style.css'; // Assuming your custom styles are in style.css
-import Footer from './Footer';
+// import Footer from './Footer';
 import image1 from '../image1.jpg'; // Import the images
 import image2 from '../image2.jpg'; // Import the images
 import 'animate.css/animate.min.css';
@@ -46,8 +46,34 @@ function HomePage() {
         </Col>
       </Row>
 
-      {/* Footer */}
-      <Footer />
+      <footer className="footer fixed-bottom bg-light py-4 border-top">
+      <div className="container">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-md-4">
+            <h5>Hamza Alsiyabi</h5>
+            <p>About Us</p>
+            <p>Contact Us</p>
+          </div>
+          <div className="col-md-4">
+            <h5>Quick Links</h5>
+            <p>Home</p>
+            <p>Register</p>
+            <p>Products</p>
+          </div>
+          <div className="col-md-4">
+            <h5>Follow Us</h5>
+            <p><i className="bi bi-twitter"></i> Twitter</p>
+            <p><i className="bi bi-instagram"></i> Instagram</p>
+            <p><i className="bi bi-facebook"></i> Facebook</p>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col text-center">
+            <p>&copy; 2024 Company, Inc</p>
+          </div>
+        </div>
+      </div>
+    </footer>
     </Container>
   );
 }
